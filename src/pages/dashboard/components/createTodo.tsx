eab6181c5d96cfc0d11d6e4d9a35645d722685e9
awallemo <awallemo_98@hotmail.com>
2023-10-13 13:33:02 +0200

    refactor: add user when creating todo

--- src/pages/dashboard/components/createTodo.tsx
+++ src/pages/dashboard/components/createTodo.tsx
@@ -1,8 +1,9 @@
-import {useContext, useState} from "react";
-import {Textarea, Input, Box, Button} from '@chakra-ui/react'
+import {useContext, useEffect, useState} from "react";
+import {Textarea, Input, Box, Button, Select} from '@chakra-ui/react'
 import {Todo} from "../../../models/todoModel.ts";
-import {TodoContext} from "../../../contexts/todoContext.ts";
+import {DashboardContext} from "../../../contexts/dashboardContext.ts";
 import styled from "styled-components";
+import {User} from "../../../models/userModel.ts";
 
 const FormContainer = styled.div`
   max-width: 300px; 
@@ -12,11 +13,12 @@ const FormContainer = styled.div`
 `
 
 export const CreateTodo = () => {
-    const {todos, setTodos} = useContext(TodoContext)
+    const {todos, setTodos, users} = useContext(DashboardContext)
     const [title, setTitle] = useState<string>('');
     const [description, setDescription] = useState<string>('');
+    const [user, setUser] = useState<string>()
     const [error, setError] = useState<boolean>();
-    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [errorMessage, setErrorMessage] = useState<string>('')
     const invalidInputHandler = (firstInput : string, secondInput : string) => {
 
         if (firstInput === '' || secondInput === '') {
@@ -34,11 +36,11 @@ export const CreateTodo = () => {
         if (!title || !description) {
             return false
         }
-        const newTodo = {
-            title: title, // Set your actual title
-            description: description, // Set your actual description
-            created: new Date()
-        } as Todo;
+        // const newTodo = {
+        //     title: title, // Set your actual title
+        //     description: description, // Set your actual description
+        //     created: new Date()
+        // } as Todo;
 
         await fetch('http://127.0.0.1:8090/api/collections/Todos/records',{
             method: 'POST',
@@ -46,11 +48,34 @@ export const CreateTodo = () => {
                 'Content-Type':'application/json'
             },
             body: JSON.stringify(
-                newTodo
+                {
+                    title: title,
+                    description: description,
+                    created: new Date(),
+                    AssignedUser: users?.find((u) => u.name === user)?.id
+                }
             )
         })
-        todos && setTodos([...todos, newTodo]);
+        console.log(users?.find((u) => u.name === user)?.id)
+        todos && setTodos([...todos, {title: title, description: description, created: new Date()} as Todo]);
+
+        await fetch('http://127.0.0.1:8090/api/collections/Users/records/',{
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    name: user,
+                    assignedTodos: todos?.find((t) => t.title === title)?.id //Har ikke fått id enda så er undefined her. Utforsk tanken med å kjøre en useEffect som trigges av den andre await fetchen kanskje?
+                }
+            )
+        })
+        console.log(user)
+        console.log(title)
+        console.log(todos?.find((todo) => todo.title === title))
     }
+
     return (
         <FormContainer>
             <Box mt='1'
@@ -76,11 +101,11 @@ export const CreateTodo = () => {
             />
             {error && <p>{errorMessage}</p>}
 
-            {/*<Select placeholder='Select option' onChange = {(e) => setUser(e.target.value)}>*/}
-            {/*    {props.users?.map((user: any, index: number)=> {*/}
-            {/*        return <option key={index} value={user} >{user.Name}</option>*/}
-            {/*    })}*/}
-            {/*</Select>*/}
+            <Select placeholder='Select option' value={user} onChange = {(e) => setUser(e.target.value)}>
+                {users?.map((user: any, index: number)=> {
+                    return <option key={index} value={user.name} >{user.name}</option>
+                })}
+            </Select>
             <Button colorScheme='teal' size='sm' onClick={() => addTodo()}>
                 Create note
             </Button>
